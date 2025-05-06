3c80e7c48d3e4aa7cc66b9c774f8a89bd4cd0012
Nir Home <nirfridm@gmail.com>
2025-05-07 01:08:03 +0300

    Updating Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\RedirectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/redirect', [RedirectController::class, 'handle']);     // GET /api/redirect?keyword=...
+Route::get('/retrieve_original/{our_param}', [ApiController::class, 'retrieve']);
+Route::post('/refresh', [ApiController::class, 'refresh']);
