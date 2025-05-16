54f307b77d4342ddf59bee3f8a741c79bc4bde56
Nir Home <nirfridm@gmail.com>
2025-05-16 09:30:32 +0300

    Product Migration ! - Laravel 12 + Vite + React

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,25 @@
-// File: resources/js/app.jsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import Dashboard from './components/Dashboard.js'
+import '../css/app.css';
+import './bootstrap';
 
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<Dashboard />)
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
