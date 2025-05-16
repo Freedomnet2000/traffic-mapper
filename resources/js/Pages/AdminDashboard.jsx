b30a5bf24ee36347e3dc68941fcefb46920cbccd
Nir Home <nirfridm@gmail.com>
2025-05-16 23:06:50 +0300

    Product Migration ! - aplying React User-MGMT

---
+++ resources/js/Pages/AdminDashboard.jsx
@@ -0,0 +1,8 @@
+// File: resources/js/Pages/AdminDashboard.jsx
+export default function AdminDashboard() {
+    return (
+        <div className="p-6 text-xl font-bold text-indigo-600">
+            Welcome to the Admin Dashboard!
+        </div>
+    );
+}
