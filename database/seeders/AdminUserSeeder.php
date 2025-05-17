cd47bf5c8c14a6a6bc2a01dd6b7573991c4b028e
Nir Home <nirfridm@gmail.com>
2025-05-17 22:26:27 +0300

    DB Seeder - adding admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@testmail.me'],
+            [
+                'name' => 'useradmin',
+                'password' => Hash::make('Ua741236'),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use AdminUserSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            AdminUserSeeder::class,
         ]);
     }
 }
