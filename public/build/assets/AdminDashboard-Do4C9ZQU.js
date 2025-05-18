9ee8d9c13395aad95959eba6d21c546b72f91394
Nir Home <nirfridm@gmail.com>
2025-05-18 04:18:25 +0300

    Build - updating dashboard

---
+++ public/build/assets/AdminDashboard-Do4C9ZQU.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-B95kNC5S.js";function e(){return t.jsx("div",{className:"p-6 text-xl font-bold text-indigo-600",children:"Welcome to the Admin Dashboard!"})}export{e as default};
--- public/build/assets/AdminDashboard-OWyTDRY7.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BBUOm-v-.js";function e(){return t.jsx("div",{className:"p-6 text-xl font-bold text-indigo-600",children:"Welcome to the Admin Dashboard!"})}export{e as default};
