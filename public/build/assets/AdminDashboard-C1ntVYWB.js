4d2c14751e37508b1a571d6b5a86f6fb3ebaef51
Nir Home <nirfridm@gmail.com>
2025-05-18 12:18:36 +0300

    Build - New Dashboard

---
+++ public/build/assets/AdminDashboard-C1ntVYWB.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-D-HvXw_r.js";function e(){return t.jsx("div",{className:"p-6 text-xl font-bold text-indigo-600",children:"Welcome to the Admin Dashboard!"})}export{e as default};
