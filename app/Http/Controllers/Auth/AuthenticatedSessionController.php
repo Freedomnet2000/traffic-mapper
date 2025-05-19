84985e9091e3ef9430220fdfde43588f2e67fe00
Nir Home <nirfridm@gmail.com>
2025-05-20 00:57:44 +0300

    https - AuthenticatedSessionController - debug

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,11 +28,19 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)//: RedirectResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        return response()->json([
+            'redirect_to' => route('dashboard'),
+            'url' => url('/dashboard'),
+            'app_url' => config('app.url'),
+            'secure' => app()->environment('production'),
+            'scheme_forced' => URL::to('/'),
+        ], 200);
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
